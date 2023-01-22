f454ad255ca74de06af2054d384795afeda59d94
Sachin <sachinverma780588@gmail.com>
2023-01-22 13:31:04 +0530

    call and apply and bind

---
+++ Project/portpolio website/script.js
@@ -0,0 +1,50 @@
+
+// //call,apply,and bind method in js
+// //call
+// let name = {
+//     firstName : "Sachin",
+//     lastName : "Verma",
+//     PrintFullName: function(){
+//         console.log(this.firstName+ " "+ this.lastName);
+//     }
+// }
+// name.PrintFullName();
+// //Sachin Verma
+
+// let name2 = {
+//     firstName : "Sachin",
+//     lastName : "Tendulkar",   
+// }
+// //call
+
+// name.PrintFullName.call(name2);
+
+
+//Sachin Tendulkar
+
+let name = {
+    firstName : "Sachin",
+    lastName : "Verma",
+}
+//seperate method
+   let PrintFullName =  function( hometown,state ){
+        console.log(this.firstName+ " "+ this.lastName +" from "+ hometown+" "+state);
+    }
+
+PrintFullName.call(name,"lucknow","u.p");
+
+let name2 = {
+        firstName : "Sachin",
+         lastName : "Tendulkar",   
+
+
+            }
+            //call and apply method
+            PrintFullName.call(name2,"mumbai","Maharastra")//call as indevisually and seperated
+            PrintFullName.apply(name2,["mumbai","Maharastra"])//call as a arraay list
+
+            //bind method
+//don't directly in voke the method,it just give copy of same method which can be invoked latter
+            let PrintMyName = PrintFullName.bind(name2,"mumbai","Maharastra");
+            console.log(PrintMyName);
+            PrintMyName();
