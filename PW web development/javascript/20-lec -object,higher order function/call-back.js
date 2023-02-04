336320b4dcb0b86d37d12405319325c0f5f53c93
Sachin <92576557+Sachim91@users.noreply.github.com>
2023-02-04 15:24:43 +0000

    basic

---
+++ PW web development/javascript/20-lec -object,higher order function/call-back.js
@@ -0,0 +1,59 @@
+// Higher Order Function
+//A function takes inside a function as a parameter is higher order function
+//call back function
+
+// let isEven = (ele) =>{
+//     return ele % 2 == 0;
+// }
+
+// var value = [2,4,6].every(isEven);
+// console.log(value);
+// //o/p - true;
+
+// var value = [2,3,4,6].every(isEven);
+// console.log(value);
+// //o/p=false;
+
+
+
+//                                                        mini Project 
+//function to Square
+ 
+
+//for square
+// const callback = (n) =>{
+//     return n**2;
+// };
+// //function to multiple square with other number
+// function  cube (callback,n){
+//     return callback(n)*n;
+// }
+
+// console.log((cube(callback,3)));
+
+
+//set time interval function
+function course(){
+    console.log("Welcome to sachin zone");
+}
+//setInterval(course);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PW web development/javascript/21-lec-map and set/map.js
@@ -0,0 +1,92 @@
+//changing datatype
+
+//String to number
+
+// let fakenum = "25";
+// console.log(typeof faken um);
+
+
+// let orgNum = Number(fakenum);
+// console.log(typeof orgNum);
+
+
+
+//new 
+
+//new is a keyword
+
+// let person = {
+//     name : "Anurag"
+
+// }
+
+// console.log(person);
+
+
+// let person = new Object();
+// person.name = "Anurag";
+
+// console.log(person);
+
+
+// let person = function(firstname,lastname){
+//     this.firstname = firstname;
+//     this.lastname = lastname;
+// }
+// let sachin = new person("sachiin","verma")
+// console.log(sachin);
+
+
+
+
+
+
+
+//                                          Sets and Maps
+
+//set
+
+
+
+// let arr = [2,3,5,4,5,9,6,4];
+
+// let num = new Set(arr);
+// console.log(num);
+// num.add(1);
+// console.log(num);
+// num.delete(1)
+// console.log(num);
+// console.log(num.has(7));
+
+// //maps
+// //   it store value in key form o key value pair
+
+
+// let mewmao = new Map();
+// mewmao.set("1","one")
+// console.log(mewmao);
+
+
+//{"1","one"}  //object
+//{1,2,,3,,3,4,4}      //set
+//{"1" =>'one'}       //map
+
+
+
+
+
+
+
+
+let city = [['india','delhi']
+,['surat','jaipur']];
+
+
+let newmap = new Map(city);
+
+console.log(newmap);
+
+
+
+
+
