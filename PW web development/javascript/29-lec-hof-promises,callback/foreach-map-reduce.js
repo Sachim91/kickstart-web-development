71031a8354159f9c80ae06d73f6d7f34bff27c07
Sachin <92576557+Sachim91@users.noreply.github.com>
2023-02-10 06:53:41 +0000

    for ach map sort

---
+++ PW web development/javascript/29-lec-hof-promises,callback/foreach-map-reduce.js
@@ -0,0 +1,34 @@
+
+//for each
+// let array = [2,4,5,6,2,6,7]
+
+
+// array.forEach(element => {
+//     console.log(element);
+    
+// });
+
+//  map--iterate the array elemnt and modify the array element
+
+ const num = [2,3,4,4,5,5,8];
+// const abc = num.map((val) => {
+//    let c= val * val;
+//    return c;
+// })
+// console.log(abc);
+
+
+//                                               reduce
+
+//  const choc= [1,2,3,4,5];
+//  const wrap = choc.reduce((acc,pos) => acc+pos,2 );
+//  console.log(wrap);
+
+
+
+//                                       sort
+
+
+// const name = ["sachin", "kuldeep", "Mahendra"]
+
+// console.log(name.sort());
