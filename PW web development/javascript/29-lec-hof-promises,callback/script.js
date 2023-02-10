2a8a3a65e6edbe3c322b511d099cc1f9fa4712d6
Sachin <92576557+Sachim91@users.noreply.github.com>
2023-02-10 05:48:08 +0000

    Higher Order Function

---
+++ PW web development/javascript/29-lec-hof-promises,callback/script.js
@@ -0,0 +1,48 @@
+//hof  
+
+//CallBack
+
+// function greet(userName,callback){
+//     console.log("HEllo",userName);
+//     callback();
+// }
+
+// function callback(){
+//     console.log("I am call back");
+// }
+// greet("sachin",callback);
+
+
+// const callback = (n) =>{
+//       return n ** 2;
+// };
+
+
+// function squ(callback,n) {
+//       return callback(n)
+//      }
+
+// console.log(squ(callback,5)); 
+
+
+//                                                                  settimeout
+    
+// function courseName(){
+//     console.log("Welcome to JS Bootcamp");
+// }
+
+// setTimeout(courseName,4000);
+
+// setTimeout(() => {
+//   console.log("Hello Sachin");  
+// },3000);
+
+
+//                                                          setinterval
+
+
+// setInterval(() => {
+//     console.log("hello");
+// }, 3000);
+
+console.log("hello");
